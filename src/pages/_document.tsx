0c9c32bf07d149b27d24d38c4bb353c9dd77802b
Adner Scarpelini <adner.scarpelini@gmail.com>
2021-02-23 09:18:21 -0300

    Ajuste CSS

---
+++ src/pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+// Esse arquivo serve para adicionar coisas ao HTML global da aplicação
+// Aqui coloca os links de fontes globais por exemplo
+//Resumindo: aqui vai tudo que é estatico, que é aproveitado em todas as rotas
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
